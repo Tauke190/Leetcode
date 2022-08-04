6a6338263dc0fbc31a93484849815fecffe2001d
Avinash Gyavali <avinashgyavali190@gmail.com>
2022-08-04 15:22:29 +0545

    Update 547-number-of-provinces.cpp

--- 547-number-of-provinces/547-number-of-provinces.cpp
+++ 547-number-of-provinces/547-number-of-provinces.cpp
@@ -1,5 +1,21 @@
 class Solution {
 public:
+    /* Difficulty - Medium 
+    Type - Graph Union Find
+    Attmept 1  -  Almost done it 80%. Algorithm was right but could not implement it
+
+
+
+    Algorithm
+    1.Iterate through the matrix
+    2.Make a parent array which will hold the representative or the parent of the element
+    3.Populate with itself because all the nodes will be their own parent
+    4.Find two such cities which are connected but not connected with itself
+    5.The ith and jth index will be our node of the graph
+    6.Union find them , if their parent are different then parent(x) = y and they will be share a common parent or be in a same subset
+    7.Find function is a recursive function which finds the main parent of the element
+    8.If find gives the element itself , that is the base condition , else it searches for parent of parent
+    */
     int findCircleNum(vector<vector<int>>& isConnected) 
     {
         int n = isConnected.size();
