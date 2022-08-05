350091c10a1f02d9cc0cbbaacd6c7582b8d97e85
Avinash Gyavali <avinashgyavali190@gmail.com>
2022-08-05 14:50:17 +0545

    Time: 15 ms (99.06%), Space: 13.5 MB (75.60%) - LeetHub

--- 207-course-schedule/207-course-schedule.cpp
+++ 207-course-schedule/207-course-schedule.cpp
@@ -1,3 +1,24 @@
+/* Difficulty - Medium 
+    Type - DFS / Graph Coloring
+    Attmept 1  -  Spent 5 hours on it , Could not do it
+    
+
+    Time Complexity : 0(V+E)
+    Space Compexity : 0(N)
+    
+    Algorithm
+    1.Make a adjancency list
+    2.Make a isvisted array with 3 colors , explored , exploring , unexplored
+    3.Run dfs on each of the unexplored node
+    4.If dfs gives false , it mean it has cycle in its graph , return false
+    5.Each recursive dfs function carries the node, adjlist, isvisted array
+    6.Make the current node as processing 
+    7.Look at its adjanent/neighbour node , it if its exploring , return false because it has cycle
+    8.For the unexplored neighbour/adjance node, run dfs on that node and see if it has cycle or not
+    9.For node with no neighbours
+    10.Means it does not have prereqs and mark as visited and dfs will return true means no cycle
+    */
+
 class Solution {
     
     
