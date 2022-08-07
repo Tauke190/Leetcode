03192e12add0471849994f8f067a76f12762a263
Avinash Gyavali <avinashgyavali190@gmail.com>
2022-08-07 18:30:38 +0545

    Time: 214 ms (20.58%), Space: 67.8 MB (10.92%) - LeetHub

--- 53-maximum-subarray/53-maximum-subarray.cpp
+++ 53-maximum-subarray/53-maximum-subarray.cpp
@@ -5,24 +5,18 @@ Attmept 1  - Fucking 3 hours
 Attemp  2  - Fucking gave up and watched a video bitch
 
 
-Algorithm(Only for positive numbers)
+Algorithm
+1. Keep the maxsum the first element in the array
 1. Keep on increasing its sum
 2. If its max store it somewhere 
 3. If the currentsum < 0 , then it does not subarray till that index does not contribute anymore ,reset it
 4. Retun the maxsum and enjoy
 
 
-Algorithm(Only for positive numbers)
-1. First check if the current sum is negative, if it is currentsum = 0;
-2. Increase the currentsum in the loop
-3. If the currentsum is 
+
+
 
 */
-
-
-
-
-
 
 
 
@@ -40,7 +34,6 @@ public:
         
         for(int i = 0 ; i < N ; i++)
         {
-            
             currsum = currsum + nums[i];
             maxsum = max(maxsum,currsum);
             if(currsum < 0)
