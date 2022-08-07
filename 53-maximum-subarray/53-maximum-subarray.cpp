e1e23658f62dd75768f5b2e7cad40921ac0ced27
Avinash Gyavali <avinashgyavali190@gmail.com>
2022-08-07 18:29:07 +0545

    Time: 206 ms (25.83%), Space: 67.6 MB (99.47%) - LeetHub

--- 53-maximum-subarray/53-maximum-subarray.cpp
+++ 53-maximum-subarray/53-maximum-subarray.cpp
@@ -13,10 +13,9 @@ Algorithm(Only for positive numbers)
 
 
 Algorithm(Only for positive numbers)
-1. Keep on increasing its sum
-2. If its max store it somewhere 
-3. If the currentsum < 0 , then it does not subarray till that index does not contribute anymore ,reset it
-4. Retun the maxsum and enjoy
+1. First check if the current sum is negative, if it is currentsum = 0;
+2. Increase the currentsum in the loop
+3. If the currentsum is 
 
 */
 
@@ -41,12 +40,13 @@ public:
         
         for(int i = 0 ; i < N ; i++)
         {
+            
+            currsum = currsum + nums[i];
+            maxsum = max(maxsum,currsum);
             if(currsum < 0)
             {
                 currsum = 0;
             }
-            currsum = currsum + nums[i];
-            maxsum = max(maxsum,currsum);
         }
         return maxsum;
        
