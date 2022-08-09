c11e182a17e2c59ae21394c3802861e5a1f94ecd
Avinash Gyavali <avinashgyavali190@gmail.com>
2022-08-09 11:12:54 +0545

    Time: 195 ms (12.94%), Space: 58.9 MB (97.79%) - LeetHub

--- 11-container-with-most-water/11-container-with-most-water.cpp
+++ 11-container-with-most-water/11-container-with-most-water.cpp
@@ -1,5 +1,24 @@
+/* 
+Difficulty - Medium 
+Type - Array / Two pointers
+Attmept 1  - 30 Mins bitch ( Solved AF)
 
 
+
+Time  Complexity: 0(N)
+Space Complexity: 0(1)
+
+Algorithm
+1. The idea is search for the container with highest possible vertical lines with farthest distance
+2. To do that , we find area of all  possible the largest container
+3. To do that , we use two pointer approach
+4. Start with the most left and most right pointer
+5. Calcualte the area
+6. To go to next largest container , we see which is smaller among pointers
+7. And we shift the smaller pointers and calculate area again
+8. the max area of all the containers we have calculated is the answer
+9. Return the max area
+*/
 
 class Solution {
 public:
