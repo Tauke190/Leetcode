9477ca27021398f7cc4050fb42c0cc89b2bbf2d2
Avinash Gyavali <avinashgyavali190@gmail.com>
2022-08-10 12:15:27 +0545

    Time: 102 ms (5.02%), Space: 13.8 MB (71.85%) - LeetHub

--- 152-maximum-product-subarray/152-maximum-product-subarray.cpp
+++ 152-maximum-product-subarray/152-maximum-product-subarray.cpp
@@ -1,3 +1,24 @@
+/* 
+Difficulty - Medium
+Type - Array / Two pointers
+Attmept 1  - Could Not Solve it
+Attempt 2  - Solved it watching a youtube video
+
+
+
+Time  Complexity: 0(N)
+Space Complexity: 0(1)
+
+Algorithm
+1. As you scan through the array
+2. You need to maintain the current minimum number till that index
+3. You need to matintain the current maximum number till that index
+4. When you calculate max element store it in temp before you calculate the minimum number
+5. The finalmax is the max between the currMax and the finalmax
+6. Return the max 
+*/
+
+
 class Solution {
 public:
     int maxProduct(vector<int>& nums)
