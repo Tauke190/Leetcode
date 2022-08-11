a0245388d9b8a73f15463df09a42b487c173befa
Avinash Gyavali <avinashgyavali190@gmail.com>
2022-08-11 13:15:13 +0545

    Time: 42 ms (39.52%), Space: 13 MB (54.44%) - LeetHub

--- 1143-longest-common-subsequence/1143-longest-common-subsequence.cpp
+++ 1143-longest-common-subsequence/1143-longest-common-subsequence.cpp
@@ -1,3 +1,24 @@
+/* 
+Difficulty - Easy 
+Type - Dynamic Programming
+Attmept 1  - Could not solve it (I am fucking dump)
+
+
+
+
+Time  Complexity: 0(m*n)
+Space Complexity: 0(m*n)
+
+Algorithm
+1. Always start from the start ( The Dp tabulation bottom up approach)
+2. In the double loop , check if the last characters are same
+3. If equal subsequence length would increase by 1  + the results that comes after that , so we dont need to calculate it. dp[i][j] = 1 + dp[i+1][i+1];
+4. If not equal than it is the max of the subsequence that was calculated before it (Its right and down neighbours)
+5.Return dp[0][0] and Enjoy
+*/
+
+
+
 class Solution {
 public:
     int longestCommonSubsequence(string text1, string text2)
