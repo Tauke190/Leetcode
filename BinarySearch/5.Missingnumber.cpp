0ff36c022be9c6cc057af162503d09486cb6469b
Avinash Gyavali <avinashgyavali190@gmail.com>
2022-08-26 13:45:33 +0545

    Update 5.Missingnumber.cpp

--- BinarySearch/5.Missingnumber.cpp
+++ BinarySearch/5.Missingnumber.cpp
@@ -68,3 +68,46 @@ public:
         return totalsum - vectorsum;               // The difference is the missing element in the vector
     }
 };
+
+
+// Method 3 : Cyclic Sort 0(N)
+
+class Solution {
+public:
+    int missingNumber(vector<int>& nums) 
+    {
+        
+        int N = nums.size();
+        int curr_pos = 0;
+        
+        
+        while(curr_pos < N)                                 
+        {
+            if(nums[curr_pos] < N && nums[curr_pos] != curr_pos)           // If the current element is not in the correct position, swap it to the current position
+            {
+                int index = nums[curr_pos];
+                swap(nums[curr_pos], nums[index]);
+            }
+            else
+            {
+                curr_pos++;                                               // If th current element is in the correct positon , increse the curren_position
+            }
+        }
+        
+        for(auto x : nums)
+        {
+            cout<<x;
+        }
+        
+        for(int i = 0 ; i < nums.size(); i++)
+        {
+            if(nums[i] != i)                                            // after sorter , check for the missing elements by comparing the loop number
+            {
+                return i;
+            }
+        }
+        
+        return nums.size();
+       
+    }
+};
