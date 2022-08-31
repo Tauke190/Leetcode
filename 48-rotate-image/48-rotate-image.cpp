8ac0a2599898ddecb044bcddde76a762fd99d2ed
Avinash Gyavali <avinashgyavali190@gmail.com>
2022-08-31 18:54:50 +0400

    Time: 0 ms (100.00%), Space: 7.1 MB (73.72%) - LeetHub

--- 48-rotate-image/48-rotate-image.cpp
+++ 48-rotate-image/48-rotate-image.cpp
@@ -1,21 +1,31 @@
+/* 
+Difficulty - Medium 
+Type - Matrix 
+Attmept 1  - Could Not do it 
+Attempt 2  - Could do it with a small hint
+
+
+
+Algorithm
+1. Change the matrix into the transpose of the matrix using swapping (will be easy)
+2. Reverse the orders of the row or column in the according to the rotation in the question
+3. Done
+*/
+
 class Solution {
 public:
     void rotate(vector<vector<int>>& matrix) 
     {
-        
-        
-        
-        int N = matrix.size();
-        
+    
+        int N = matrix.size();              
         
         for(int col = 0 ; col < N ; col++)
         {
             for(int row = col ; row < N ; row++)
             {
-               swap(matrix[row][col] , matrix[col][row]);
+               swap(matrix[row][col] , matrix[col][row]);                   // Transforming the matrix into transpose matrix
             }
         }
-        
         
         for(int row = 0 ; row < N ; row++)
         {
@@ -24,7 +34,7 @@ public:
             
             while(left < right)
             {
-                swap(matrix[row][left],matrix[row][right]);
+                swap(matrix[row][left],matrix[row][right]);                 // Reversing the order of every row in the matrix
                 left++;
                 right--;
             }
